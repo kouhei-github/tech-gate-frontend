57f25a6ffd0689cdd67b9cbae664940d60e72442
nagamatsu-k <nagamatsu-k@dym.jp>
2023-08-23 15:22:22 +0900

    jwtトークンをローカルストレージに保存できるようにした

--- pages/test.vue
+++ pages/test.vue
@@ -1,17 +1,13 @@
 <script setup lang="ts">
-import {useCounterStore} from '~/stores/jwt_token'
+import {useJwtTokenStore} from '~/stores/jwt_token'
 
-const counter = useCounterStore()
+const jwt = useJwtTokenStore()
 </script>
 
 <template>
-  {{ counter.counter }}
-  <button @click="counter.counter++">button</button>
+  {{ jwt.jwtToken }}
 </template>
 
 <style scoped>
 
 </style>
-9425  git stash push src/domain/README.md -m "README.mdの更新"
-9426  git stash pop stash@{0}
-9427  git stash list

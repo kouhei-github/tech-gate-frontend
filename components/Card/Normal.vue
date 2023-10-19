a1d4210f333c871ce238ed39971b490ff91a4d82
kouhei-github <kohei0801nagamatsu@gmail.com>
2023-10-19 17:25:58 +0900

    カードコンポーネントにユーザーアクション追跡機能を追加
    このコミットでは、Cardコンポーネントにユーザーアクション追跡機能が追加されました。この新機能は、投稿に対する「マーク」「コメント」「いいね！」などのユーザーアクションをトラッキングし、リアクティブに更新します。userAction'という新しいリアクティブオブジェクトが作成され、アクションのトリガーとなるメソッド（'mark'、'comment'、'good'）が更新されました。また、投稿が'userAction'に含まれる場合、その投稿を'like'または'bookmarked'としてマークするための'include'というヘルパー関数が導入されました。このアップデートは、投稿に対するユーザーのアクションを正確に反映することで、よりインタラクティブで反応の良いUIをユーザーに提供します。

--- components/Card/Normal.vue
+++ components/Card/Normal.vue
@@ -4,8 +4,22 @@ import {Post, postUserBookMarked, postUserLiked} from '~/models/article'
 
 const props = defineProps<{recommendImage: Post}>();
 const recommendImage = computed(() => props.recommendImage)
+
+type UserAction = {
+  good: number[],
+  comment: number[],
+  mark: number[]
+}
+
+const userAction = reactive<UserAction>({
+  good: [] as number[],
+  comment: []as number[],
+  mark: []as number[]
+})
+
 const mark = async (id: number) => {
-    await postUserBookMarked({articleId: id})
+  await postUserBookMarked({articleId: id})
+  userAction.mark.push(id)
 }
 
 const comment = (id: number) => {
@@ -14,6 +28,7 @@ const comment = (id: number) => {
 
 const good = async (id: number) => {
   await postUserLiked({articleId: id})
+  userAction.good.push(id)
 }
 
 const router = useRouter()
@@ -24,6 +39,15 @@ const moveToOtherSite = (page: string) => {
 
 const moveTo = (page: string) => {
     router.push(`/categories/${page}`)
+}
+
+const include = (isFlag: boolean, postId: number, action: "good" | "mark") => {
+  switch (action){
+    case "good":
+      return isFlag || userAction.good.includes( postId );
+    case "mark":
+      return isFlag || userAction.mark.includes( postId );
+  }
 }
 
 </script>
@@ -50,7 +74,7 @@ const moveTo = (page: string) => {
 
         <div class="flex items-center justify-center space-x-2 z-20" @click.stop="mark(props.recommendImage.id)">
           <div class="flex items-center justify-center"  @click.stop="mark(props.recommendImage.id)">
-              <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 hover:bg-white" :class="{'text-[#3595E8] fill-[#3595E8]': recommendImage.now_bookmarked, 'text-[#B6BBC0]' : !recommendImage.now_bookmarked}" width="18" height="18" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M9 4h6a2 2 0 0 1 2 2v14l-5 -3l-5 3v-14a2 2 0 0 1 2 -2"></path></svg>
+              <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 hover:bg-white" :class="{'text-[#3595E8] fill-[#3595E8]': include(recommendImage.now_bookmarked, recommendImage.id, 'mark'), 'text-[#B6BBC0]' : !include(recommendImage.now_bookmarked, recommendImage.id, 'mark')}" width="18" height="18" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M9 4h6a2 2 0 0 1 2 2v14l-5 -3l-5 3v-14a2 2 0 0 1 2 -2"></path></svg>
           </div>
           <div class="flex items-center justify-center" @click.stop="comment(props.recommendImage.id)">
             <svg class="w-5 h-5 text-[#B6BBC0] hover:bg-white" width="16" height="16" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M3 20l1.3 -3.9a9 8 0 1 1 3.4 2.9l-4.7 1"></path></svg>
@@ -58,8 +82,8 @@ const moveTo = (page: string) => {
           </div>
 
           <div class="flex items-center justify-center" @click.stop="good(props.recommendImage.id)">
-            <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 hover:bg-white" :class="{'text-[#3595E8] text-lg': recommendImage.now_liked, 'text-[#B6BBC0]' : !recommendImage.now_liked}" width="18" height="18" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M7 11v8a1 1 0 0 1 -1 1h-2a1 1 0 0 1 -1 -1v-7a1 1 0 0 1 1 -1h3a4 4 0 0 0 4 -4v-1a2 2 0 0 1 4 0v5h3a2 2 0 0 1 2 2l-1 5a2 3 0 0 1 -2 2h-7a3 3 0 0 1 -3 -3"></path></svg>
-            <span class="ml-1" :class="{'text-[#3595E8]': recommendImage.now_liked, 'text-[#B6BBC0]' : !recommendImage.now_liked}">{{recommendImage.good_num}}</span>
+            <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 hover:bg-white" :class="{'text-[#3595E8] text-lg': include(recommendImage.now_liked, recommendImage.id, 'good'), 'text-[#B6BBC0]' : !include(recommendImage.now_liked, recommendImage.id, 'good')}" width="18" height="18" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M7 11v8a1 1 0 0 1 -1 1h-2a1 1 0 0 1 -1 -1v-7a1 1 0 0 1 1 -1h3a4 4 0 0 0 4 -4v-1a2 2 0 0 1 4 0v5h3a2 2 0 0 1 2 2l-1 5a2 3 0 0 1 -2 2h-7a3 3 0 0 1 -3 -3"></path></svg>
+            <span class="ml-1" :class="{'text-[#3595E8]': include(recommendImage.now_liked, recommendImage.id, 'good'), 'text-[#B6BBC0]' : !include(recommendImage.now_liked, recommendImage.id, 'good')}">{{recommendImage.good_num}}</span>
           </div>
 
         </div>

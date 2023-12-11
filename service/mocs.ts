d5c722e5dbb2167e33d864d86e68009d169b21db
kouhei-github <kohei0801nagamatsu@gmail.com>
2023-12-11 12:43:44 +0900

    Add properties to mocked data in mocs.ts
    This commit expands the mocked data in 'mocs.ts' by importing the 'Post' type from '~/models/article' and adding several new fields to the 'data' object. This allows us to more fully mimic the potential response from the API, aiding in the development and testing process.

--- service/mocs.ts
+++ service/mocs.ts
@@ -1,6 +1,9 @@
-export function serachTagResponseMocData() {
+import type {Post} from '~/models/article'
+
+export function serachTagResponseMocData():{data: Post[], search_tag_url: string, related_tags: {name: string, image: string}[]} {
   return {
     data: [{
+      id: 1,
       image: "/images/download.webp",
       url: "",
       title: "",
@@ -9,7 +12,11 @@ export function serachTagResponseMocData() {
       site: {image: "/images/download.webp", name: ""},
       comment: 0,
       good: 0,
-      book_marked: false
+      book_marked: false,
+      good_num: 0,
+      comment_num: 0,
+      now_bookmarked: false,
+      now_liked: false,
     }],
     search_tag_url: "",
     related_tags: [{name: "", image: "/images/download.webp"}]
